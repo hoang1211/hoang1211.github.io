b3a91f2d81f17801b9331257b851256905e99eae
hoang1211 <rovpc1211@gmail.com>
2021-03-30 01:14:54 +0700

    fix

---
+++ homeproject/javacoban/script.js
@@ -0,0 +1,38 @@
+// ------Bài 1---------------
+// Viết chương trình nhập vào một giá trị đo độ dài ở đơn vị cm, tính và in ra giá trị tương ứng ở
+// các đơn vị mm, m, km
+// ------Loi Giai------------
+// let donVi = Number(prompt("Nhap so cm: "));
+// function donViMm(cm) {
+//   return cm * 10;
+// }
+// function donViM(m) {
+//   return m * 0.01;
+// }
+// function donViKm(km) {
+//   return km * 0.001;
+// }
+// console.log(
+//   "So Mm : " +
+//     donViMm(donVi) +
+//     " So M : " +
+//     donViM(donVi) +
+//     " So KM : " +
+//     donViKm(donVi)
+// );
+// ------Bài 2---------------
+// Viết chương trình nhập vào một giá trị nhiệt độ ở đơn vị Celsius, in ra nhiệt độ ở đơn vị
+// Fahrenheit và Kevin tương ứng
+// ------Loi Giai------------
+// let nhietDoC = Number(prompt("Nhap nhiet do C: "));
+// function nhietDoF(doF) {
+//   return doF * 1.8 + 32;
+// }
+// function nhietDoK(doK) {
+//   return doK + 273.15;
+// }
+// console.log(" Do F : " + nhietDoF(nhietDoC) + " Do K : " + nhietDoK(nhietDoC));
+// ------Bài 3---------------
+// Viết chương trình nhập thời gian hiện tại tính theo số giây từ 0h, tính và in ra giá trị
+// giờ/phút/giây tương ứng theo định dạng h:m:s
+// ------Loi Giai------------
