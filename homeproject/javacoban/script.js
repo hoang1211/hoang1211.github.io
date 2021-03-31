15a49034a6ac0354562ecc33f72242ee650fc143
hoang1211 <rovpc1211@gmail.com>
2021-03-31 11:53:19 +0700

    cap nhap lab 10

--- homeproject/javacoban/script.js
+++ homeproject/javacoban/script.js
@@ -54,3 +54,50 @@
 // let soPhut = parseInt((soGiayNhapVao % 3600) / 60);
 // let soGiay = (soGiayNhapVao % 3600) % 60;
 // console.log("Thoi gian la: " + soGio + "h:" + soPhut + "m:" + soGiay);
+// ----bai1 -----
+// let a = Number(prompt("nhap so 1: "));
+// let b = Number(prompt("nhap so 2: "));
+// a > b && console.log(a);
+// a < b && console.log(b);
+// a == b && console.log("2 so bang nhau");
+// bai2
+// let str = prompt("nhap vao 1 chuoi : ");
+// str == "Ba" && console.log("Thay Ba dep trai");
+// str != "Ba" && console.log("Thay Ba van dep trai");
+// bai 3
+// let a = Number(prompt("nhap so : "));
+// if (a % 2 == 0) {
+//   alert("So chan");
+// } else {
+//   alert("So le");
+// }
+// bai 4
+// let so = Number(prompt("Nhap so : "));
+// if (2 < so && so < 6) {
+//   alert("mua xuan");
+// } else if (5 < so && so < 8) {
+//   alert("mua he");
+// } else if (8 < so && so < 12) {
+//   alert("mua thu");
+// } else {
+//   alert("mua dong");
+// }
+//bai 5
+// let so = Number(prompt("Nhap so : "));
+// if (so % 5 == 0 && so % 11 == 0) {
+//   alert("so chia het cho 5 va 11");
+// } else {
+//   alert("so khong chia het cho 5 va 11");
+// }
+// bai6
+// let a = Number(prompt("Nhap so a: "));
+// let b = Number(prompt("Nhap so b: "));
+
+// if (a == 0 && b == 0) {
+//   alert("phuong trinh vo so nghiem");
+// } else if (a == 0 && b!= 0) {
+//   alert("phuong trinh vo nghiem");
+// } else {
+//   let nghiem = -b / a;
+//   alert("nghiem la: " + nghiem);
+// }
