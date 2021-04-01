2703f231eb881f775259954d966f0907fca17bf8
hoang1211 <rovpc1211@gmail.com>
2021-04-02 00:30:31 +0700

    xong bai

--- homeproject/javacoban2/script.js
+++ homeproject/javacoban2/script.js
@@ -69,11 +69,56 @@
 
 // 3. VCT nhập một ký tự char, kiểm tra và in ra ký tự đó là nguyên hay phụ âm (tiếng Anh)
 
+// let kyTu = prompt("Nhap vao ki tu : ");
+// let charTest = kyTu.toUpperCase();
+// if (charTest >= "A" && charTest <= "Z") {
+//   if (
+//     charTest == "A" ||
+//     charTest == "E" ||
+//     charTest == "O" ||
+//     charTest == "I" ||
+//     charTest == "U"
+//   ) {
+//     console.log("Ky tu : " + kyTu + " la nguyen am");
+//   } else {
+//     console.log("Ky tu : " + kyTu + " la phu am");
+//   }
+// } else {
+//   console.log("Ban vua nhap khong phai la ky tu trong bang chu cai!!! ");
+// }
+
 // 4. VCT nhập một ký tự char, kiểm tra và in ra ký tự đó là chữ thường hay chữ in hoa
+
+// let kyTu = prompt("Nhap vao ki tu : ");
+// if (kyTu >= "a" && kyTu <= "z") {
+//   console.log(kyTu + " la chu thuong");
+// } else if (kyTu >= "A" && kyTu <= "Z") {
+//   console.log(kyTu + " la chu hoa");
+// } else {
+//   console.log(kyTu + " khong nam trong bang chu cai ");
+// }
 
 // 5. VCT nhập ba hệ số a, b, c, của phương trình bậc 2 ax^2
 // + bx + c = 0, tính và in ra nghiệm
 // phương trình đó
+
+// let a = Number(prompt("nhap so a: "));
+// let b = Number(prompt("nhap so b: "));
+// let c = Number(prompt("nhap so c: "));
+// let d = b * b - 4 * a * c;
+
+// if (a == 0) {
+//   console.log("ptrinh vo nghiem");
+// } else if (d < 0) {
+//   console.log("ptrinh vo nghiem");
+// } else if (d == 0) {
+//   let nghiem = -b / (2 * a);
+//   console.log("ptrinh co nghiem kep : " + nghiem);
+// } else {
+//   let nghiem1 = (-b + Math.sqrt(d)) / (2 * a);
+//   let nghiem2 = (-b - Math.sqrt(d)) / (2 * a);
+//   console.log("ptrinh co 2 nghiem la : " + nghiem1 + "," + nghiem2);
+// }
 
 // 6. VCT nhập số điểm point ở thang điểm 10 của sinh viên quy đổi sang thang điểm chữ:
 // - point == 10 => A
@@ -81,3 +126,18 @@
 // - point < 7.0 => C
 // - point < 5.5 => D
 // - point < 4.0 => F
+
+// let a = Number(prompt("Nhap so diem point : "));
+// if (a < 0 && a > 10) {
+//   console.log("Nhap sai diem.Diem chi nam trong thang diem 10");
+// } else if (a < 4) {
+//   console.log("Diem cua ban la F ");
+// } else if (a < 5.5) {
+//   console.log("Diem cua ban la D ");
+// } else if (a < 7) {
+//   console.log("Diem cua ban la C ");
+// } else if (a < 8.6) {
+//   console.log("Diem cua ban la B ");
+// } else {
+//   console.log("Diem cua ban la A ");
+// }
