6054178b1b885fd08af26b4f4d0841345c38b850
hoang1211 <rovpc1211@gmail.com>
2021-04-16 01:52:49 +0700

    asdas

--- homeproject/array/script.js
+++ homeproject/array/script.js
@@ -4,22 +4,42 @@
 // // Ví dụ
 // avg([1, 2, 3]); // 2.0
 // ```
-function nhapMang(array1) {
-  let check = true;
-  do {
-    let a = prompt("nhap phan tu vao mang: ");
-    let b = Number(a);
-    if (a == null || isNaN(b) == true) {
-      check = false;
-    } else {
-      array1.push(b);
-    }
-  } while (check == true);
-  return array1;
-}
-let arrayAvg = [];
-nhapMang(arrayAvg);
-console.log(arrayAvg);
+
+// function nhapMang(array1) {
+//   let check = true;
+//   do {
+//     let a = prompt("nhap phan tu vao mang: ");
+//     let b = Number(a);
+//     if (a == null || isNaN(b) == true || a == "") {
+//       check = false;
+//     } else {
+//       array1.push(b);
+//     }
+//   } while (check == true);
+//   return array1;
+// }
+let arrayAvg = [1, 2, 3];
+// nhapMang(arrayAvg);
+// console.log(arrayAvg);
+// function avgArray(array1, count) {
+//   let sum = 0;
+//   for (let i = 0; i < count; i++) {
+//     sum = sum + array1[i];
+//   }
+//   return sum / count;
+// }
+// console.log(avgArray(arrayAvg, arrayAvg.length));
+
+// function avgArray(array1) {
+//   let sum = 0;
+//   let count = 0;
+//   for (let i of array1) {
+//     sum = sum + i;
+//     count++;
+//   }
+//   return sum / count;
+// }
+// console.log(avgArray(arrayAvg));
 
 // #### 2. Cho một mảng số, viết hàm tìm giá trị lớn nhất trong mảng, kết quả trả về bao gồm giá trị và chỉ mục tương ứng
 
@@ -27,6 +47,20 @@ console.log(arrayAvg);
 // // Ví dụ
 // maxOfArr([1, 2, 3]); // {index: 2, value: 3}
 // ```
+// function maxOfArr(array1) {
+//   let max = Number.NEGATIVE_INFINITY;
+//   let count = 0;
+//   let index = 0;
+//   for (let i of array1) {
+//     if (i >= max) {
+//       max = i;
+//       index = count;
+//     }
+//     count++;
+//   }
+//   console.log("index: " + index + " value: " + max);
+// }
+// maxOfArr(arrayAvg);
 
 // #### 3. Cho một mảng số viết hàm tìm số lớn thứ 2 trong mảng
 
@@ -35,6 +69,15 @@ console.log(arrayAvg);
 // secondValue([1, 3, 2, 4, 0, 4]); // 3
 // ```
 
+// function secondValue(arraySort) {
+//   arraySort.sort((a, b) => b - a);
+//   let max = arraySort[0];
+//   let arrayDeleteMax = arraySort.filter((i) => i != max);
+//   arrayDeleteMax.sort((a, b) => b - a);
+//   return arrayDeleteMax[0];
+// }
+// let a = secondValue([1, 3, 2, 4, 0, 4]);
+// console.log(a);
 // #### 4. Viết hàm chuyển đổi một chuỗi thành dạng capitalize.
 
 // ```js
@@ -42,6 +85,15 @@ console.log(arrayAvg);
 // cap("hello world"); // "Hello World"
 // ```
 
+// function cap(text) {
+//   return text
+//     .split(" ")
+//     .map(function (word) {
+//       return word[0].toUpperCase() + word.slice(1);
+//     })
+//     .join(" ");
+// }
+// console.log(cap("hello world"));
 // #### 5. Viết hàm tìm số phần tử xuất hiện nhiều lần nhất trong mảng
 
 // ```js
