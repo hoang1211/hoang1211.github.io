cbd7beaabd3de2d864ddda97c5e6e079ae63fe5c
hoang1211 <rovpc1211@gmail.com>
2021-04-24 18:25:07 +0700

    cau 3

--- BaiGiang/table/script.js
+++ BaiGiang/table/script.js
@@ -66,7 +66,7 @@ let students = [
 const tbLength = students.length;
 function creatTable() {
   //creat table
-  const table = document.createElement("TABLE");
+  let table = document.createElement("TABLE");
   table.setAttribute("id", "table");
   document.getElementById("app").appendChild(table);
   //creat th
@@ -101,7 +101,6 @@ creatTable();
  */
 
 // table.onmouseover = table.onmouseout = handler;
-
 // function handler(event) {
 //   if (event.type == "mouseover") {
 //     event.target.style.background = "pink";
@@ -118,23 +117,84 @@ for (let i = 0; i < tbLength + 1; i++) {
   x.addEventListener("mouseover", mOver);
   x.addEventListener("mouseout", mOut);
   function mOver() {
-    document.getElementById("tr" + i).style.background = "pink";
+    this.style.background = "pink";
   }
   function mOut() {
-    document.getElementById("tr" + i).style.background = "";
+    this.style.background = "";
   }
 }
+
+let table2 = document.getElementById("table");
+let rows = table2.rows;
+// for (let i = 0; i < rows.length; i++) {
+//   rows[i].onmouseover = function () {
+//     this.style.backgroundColor = "pink";
+//   };
+//   rows[i].onmouseout = function () {
+//     this.style.backgroundColor = null;
+//   };
+// }
 
 /**
  * Thêm event click cho cột tuổi (thẻ <th>), sắp xếp bảng theo độ tuổi
  * Có thay đổi thứ tự (click 1 lần sx tăng dần, click 2 lần giảm,
  * và icon hiển thị trạng thái tăng/giảm)
  */
+rows[0].cells[1].setAttribute("id", "th_Age");
+let thClick = document.getElementById("th_Age");
+
+thClick.setAttribute("onclick", "sortAge()");
+
+function sortAge() {
+  let haveSwap = true;
+  let table1 = document.getElementById("table");
+  let dir, swapCount, shouldSwap, rowSort, i;
+  dir = "asc";
+  swapCount = 0;
+  let rowX, rowY;
+  while (haveSwap) {
+    haveSwap = false;
+    rowSort = table1.rows;
+    for (i = 1; i < rowSort.length - 2; i++) {
+      shouldSwap = false;
+      rowX = Number(rowSort[i].cells[1].innerHTML);
+      rowY = Number(rowSort[i + 1].cells[1].innerHTML);
+      if (dir == "asc") {
+        if (rowX > rowY) {
+          shouldSwap = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (rowX < rowY) {
+          shouldSwap = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwap == true) {
+      rowSort[i].parentNode.insertBefore(rowSort[i + 1], rowSort[i]);
+      haveSwap = true;
+      swapCount++;
+    } else {
+      if (swapCount == 0 && dir == "asc") {
+        dir = "desc";
+        haveSwap = true;
+      }
+    }
+  }
+  if (dir == "asc") {
+    thClick.innerHTML = 'age <i class="bi bi-sort-down-alt"></i>';
+  } else if (dir == "desc") {
+    thClick.innerHTML = 'age <i class="bi bi-sort-down"></i>';
+  }
+}
 
 /**
  * Thêm form, ô input và nút tìm kiếm
  * Cho phép nhập tên học viên, tìm kiếm và highlight học viên có tên trùng khớp (sử dụng thẻ <mark>)
  */
+
+
 
 /**
  * Thêm form, các ô input tên, tuổi, giới tính, và nút thêm
