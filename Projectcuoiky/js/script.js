692b0b984bf491b271e35817451372a9d65f16c1
hoang1211 <rovpc1211@gmail.com>
2021-07-20 23:22:54 +0700

    index

---
+++ Projectcuoiky/js/script.js
@@ -0,0 +1,11 @@
+// menu fixed desk
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop()) {
+      $("header").addClass("header--fixMenu");
+    } else {
+      $("header").removeClass("header--fixMenu");
+    }
+  });
+});
+// -----------------------
