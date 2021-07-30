ae0cd66bb1dfaa4808a4ac256625d4f7165db461
hoang1211 <rovpc1211@gmail.com>
2021-07-30 11:20:50 +0700

    fix thumb slider

--- finalproject/js/script.js
+++ finalproject/js/script.js
@@ -33,14 +33,16 @@ $(document).ready(function () {
 
 // -----------------------
 // Swiper slide
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
   spaceBetween: 10,
   slidesPerView: 4,
   freeMode: true,
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
 });
-var swiper2 = new Swiper(".mySwiper2", {
+const swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
   spaceBetween: 10,
   navigation: {
     nextEl: ".swiper-button-next",
