03c5f0603df484cb52faaa474b1500e5d94605e6
hoang1211 <rovpc1211@gmail.com>
2021-08-15 07:53:16 +0700

    filter desk product

--- finalproject/js/product.js
+++ finalproject/js/product.js
@@ -7,6 +7,8 @@ let product = [
     price: 3.5,
     region: 2,
     type: 1,
+    priceType: 4,
+    squareType: 2,
   },
   {
     id: 2,
@@ -16,6 +18,8 @@ let product = [
     price: 4.5,
     region: 3,
     type: 2,
+    priceType: 5,
+    squareType: 2,
   },
   {
     id: 3,
@@ -25,6 +29,8 @@ let product = [
     price: 6.5,
     region: 2,
     type: 2,
+    priceType: 5,
+    squareType: 3,
   },
   {
     id: 4,
@@ -34,6 +40,8 @@ let product = [
     price: 18.1,
     region: 1,
     type: 3,
+    priceType: 5,
+    squareType: 5,
   },
   {
     id: 5,
@@ -43,6 +51,8 @@ let product = [
     price: 11.5,
     region: 1,
     type: 3,
+    priceType: 5,
+    squareType: 3,
   },
   {
     id: 6,
@@ -52,6 +62,8 @@ let product = [
     price: 2.4,
     region: 3,
     type: 1,
+    priceType: 3,
+    squareType: 3,
   },
 ];
 //render
@@ -106,4 +118,123 @@ let renderProductHtml = (pd) => {
     pageProductMobile.append(mobileProduct);
   });
 };
-renderProductHtml(product);
+
+let renderProductHtmlPrice = (pd, prity) => {
+  pd.forEach(({ id, houseName, square, bedRom, priceType }) => {
+    if (prity == priceType) {
+      let deskProduct = `<div class="page__product__item--desktop col-6">
+                <a href="productitem.html?productId=${id}">
+                  <a href="productitem.html?productId=${id}"
+                    ><img src="assest/product/${id}/1.jpg" alt="" class=""
+                  /></a>
+                  <div class="page__product__item__content--desktop">
+                    <div class="content-header">Mở bán ${houseName}</div>
+                    <div
+                      class="content-font"
+                      style="margin-top: 5px !important"
+                    >
+                      Diện tích: ${square} m2 - ${bedRom} Phòng Ngủ
+                    </div>
+                    <div class="link-font" style="margin-top: 12px !important">
+                      <a
+                        href="productitem.html?productId=${id}"
+                        class="page__product__item__link--desktop"
+                        >Xem thêm</a
+                      >
+                    </div>
+                  </div>
+                </a>
+              </div>`;
+      let pageProductDesk = $("#page-product-desk-show");
+      pageProductDesk.append(deskProduct);
+      let mobileProduct = `<div class="page__product__mobile__item row">
+                <div class="col-6">
+                  <a href="productitem.html?productId=${id}"
+                    ><img src="assest/product/${id}/1.jpg" alt="" class=""
+                  /></a>
+                </div>
+                <div class="page__product__item__content--desktop col-6">
+                  <div class="content-header">Mở bán ${houseName}</div>
+                  <div class="content-font" style="margin-top: 5px !important">
+                    Diện tích: ${square} m2 - ${bedRom} Phòng Ngủ
+                  </div>
+                  <div class="link-font" style="margin-top: 12px !important">
+                    <a
+                      href="productitem.html?productId=${id}"
+                      class="page__product__item__link--desktop"
+                      >Xem thêm</a
+                    >
+                  </div>
+                </div>
+              </div>`;
+      let pageProductMobile = $("#page__product__ver--mobile");
+      pageProductMobile.append(mobileProduct);
+    }
+  });
+};
+let renderProductHtmlSquare = (pd, squarty) => {
+  pd.forEach(({ id, houseName, square, bedRom, squareType }) => {
+    if (squarty == squareType) {
+      let deskProduct = `<div class="page__product__item--desktop col-6">
+                <a href="productitem.html?productId=${id}">
+                  <a href="productitem.html?productId=${id}"
+                    ><img src="assest/product/${id}/1.jpg" alt="" class=""
+                  /></a>
+                  <div class="page__product__item__content--desktop">
+                    <div class="content-header">Mở bán ${houseName}</div>
+                    <div
+                      class="content-font"
+                      style="margin-top: 5px !important"
+                    >
+                      Diện tích: ${square} m2 - ${bedRom} Phòng Ngủ
+                    </div>
+                    <div class="link-font" style="margin-top: 12px !important">
+                      <a
+                        href="productitem.html?productId=${id}"
+                        class="page__product__item__link--desktop"
+                        >Xem thêm</a
+                      >
+                    </div>
+                  </div>
+                </a>
+              </div>`;
+      let pageProductDesk = $("#page-product-desk-show");
+      pageProductDesk.append(deskProduct);
+      let mobileProduct = `<div class="page__product__mobile__item row">
+                <div class="col-6">
+                  <a href="productitem.html?productId=${id}"
+                    ><img src="assest/product/${id}/1.jpg" alt="" class=""
+                  /></a>
+                </div>
+                <div class="page__product__item__content--desktop col-6">
+                  <div class="content-header">Mở bán ${houseName}</div>
+                  <div class="content-font" style="margin-top: 5px !important">
+                    Diện tích: ${square} m2 - ${bedRom} Phòng Ngủ
+                  </div>
+                  <div class="link-font" style="margin-top: 12px !important">
+                    <a
+                      href="productitem.html?productId=${id}"
+                      class="page__product__item__link--desktop"
+                      >Xem thêm</a
+                    >
+                  </div>
+                </div>
+              </div>`;
+      let pageProductMobile = $("#page__product__ver--mobile");
+      pageProductMobile.append(mobileProduct);
+    }
+  });
+};
+const url = new URL(window.location.href);
+console.log(url);
+let searchPrice = url.searchParams.get("price");
+let searchSquare = url.searchParams.get("square");
+if (searchPrice == null && searchSquare == null) {
+  renderProductHtml(product);
+}
+if (searchPrice != null && searchSquare == null) {
+  renderProductHtmlPrice(product, searchPrice);
+}
+if (searchSquare != null && searchPrice == null) {
+  renderProductHtmlSquare(product, searchSquare);
+}
