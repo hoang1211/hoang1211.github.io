deb9166fb2793d56f373ca6a7920160ae78216fe
hoang1211 <rovpc1211@gmail.com>
2021-09-01 11:02:20 +0700

    tab about

--- newproject/asset/js/script.js
+++ newproject/asset/js/script.js
@@ -1,3 +1,4 @@
+//Menu fix
 let header = document.querySelector(".header-nav");
 let logoHeader = document.querySelector(".logo");
 window.addEventListener("scroll", (ev) => {
@@ -23,6 +24,9 @@ $(document).ready(() => {
     logoHeader.classList.remove("logo-fix");
   }
 });
+
+//Banner slider
+
 $(".banner-slider").slick({
   autoplay: true,
   autoplaySpeed: 1000,
@@ -31,4 +35,48 @@ $(".banner-slider").slick({
   fade: true,
   cssEase: "linear",
   arrows: true,
+});
+
+//Tab about
+$(document).ready(function () {
+  $("#tab-item-1").click(function () {
+    $("#tab-item-1").addClass("tab-item-active");
+    $("#tab-item-2").removeClass("tab-item-active");
+    $("#tab-item-3").removeClass("tab-item-active");
+    $("#tab-item-4").removeClass("tab-item-active");
+    $("#tab-content-1").css("display", "block");
+    $("#tab-content-2").css("display", "none");
+    $("#tab-content-3").css("display", "none");
+    $("#tab-content-4").css("display", "none");
+  });
+  $("#tab-item-2").click(function () {
+    $("#tab-item-2").addClass("tab-item-active");
+    $("#tab-item-1").removeClass("tab-item-active");
+    $("#tab-item-3").removeClass("tab-item-active");
+    $("#tab-item-4").removeClass("tab-item-active");
+    $("#tab-content-2").css("display", "block");
+    $("#tab-content-1").css("display", "none");
+    $("#tab-content-3").css("display", "none");
+    $("#tab-content-4").css("display", "none");
+  });
+  $("#tab-item-3").click(function () {
+    $("#tab-item-3").addClass("tab-item-active");
+    $("#tab-item-2").removeClass("tab-item-active");
+    $("#tab-item-1").removeClass("tab-item-active");
+    $("#tab-item-4").removeClass("tab-item-active");
+    $("#tab-content-3").css("display", "block");
+    $("#tab-content-2").css("display", "none");
+    $("#tab-content-1").css("display", "none");
+    $("#tab-content-4").css("display", "none");
+  });
+  $("#tab-item-4").click(function () {
+    $("#tab-item-4").addClass("tab-item-active");
+    $("#tab-item-2").removeClass("tab-item-active");
+    $("#tab-item-3").removeClass("tab-item-active");
+    $("#tab-item-1").removeClass("tab-item-active");
+    $("#tab-content-4").css("display", "block");
+    $("#tab-content-2").css("display", "none");
+    $("#tab-content-3").css("display", "none");
+    $("#tab-content-1").css("display", "none");
+  });
 });
